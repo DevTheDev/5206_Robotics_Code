6bf9ca09a72052f9612f69642893fb8ed8fb97ea
atwang16 <austin.t.wang@gmail.com>
2013-12-02 23:52:00 -0800

    Library containing constants for autonomous and tele-op

---
+++ consts.c
@@ -0,0 +1,46 @@
+// Robot values
+#define OPTIMAL_SPEED 75
+
+// ROBOT SPECS
+#define WHEEL_RADIUS 2
+#define WHEEL_WIDTH 17 // width between the wheels on opposite sides of robot
+#define D_RATIO 1 // the gear ratio on the drive motors
+
+#define scoredist 20;
+
+// Joystick Buttons
+#define btnX 1
+#define btnA 2
+#define btnB 3
+#define btnY 4
+
+#define btnLB 5
+#define btnRB 6
+#define btnLT 7
+#define btnRT 8
+
+// Tele-Op Constants
+#define threshhold 10
+#define constdrivereg 80.0/128.0
+#define nearblockconst 36
+#define farblockconst 20
+#define leftintakefwd 255
+#define rightintakefwd 0
+#define leftintakebck 0
+#define rightintakebck 255
+#define intakestop 127
+#define paddlespeedreg 18
+#define paddlespeedslow 8
+#define DCstop 0
+#define paddleLightThresh 50
+#define paddleratio 14.0/128.0
+
+//nxt buttons
+#define leftarrow 2
+#define rightarrow 1
+#define orangebutton 3
+#define graybutton 0
+
+//autonomous
+#define distToPend 6.0
+#define distToBridge 24.0
