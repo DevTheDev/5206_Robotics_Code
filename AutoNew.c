e0426b02749114192d21a2a92247da1401231d0c
Dev <dev.rangarajan@gmail.com>
2013-12-27 12:20:10 -0800

    Added functions to AutoNew

--- AutoNew.c
+++ AutoNew.c
@@ -14,9 +14,41 @@
 #pragma config(Servo,  srvo_S1_C2_5,    servo5,               tServoNone)
 #pragma config(Servo,  srvo_S1_C2_6,    servo6,               tServoNone)
 
+// Includes consts.c and robot.c, as well
+#include "actions.h"
+
 task main()
 {
+	// Wait for start
+	initializeRobot();
+	waitForStart();
+	// Drive to goal
+	motor[LiftFlagMtr]=100;
+	ClearTimer(T1);
+	reset();
+	drive(60);
 
+	while(!aligned() || time1[T1] < 3500){
+		if(aligned()){
+			driveTurns = nMotorEncoder[RightDr];
+			pause();
+		}
+		if(time1[T1] < 3500){
+			motor[LiftFlagMtr]=0;
+		}
+	}
+
+	// Drive to pendulum
+	turn(100);
+	wait1Msec(100);
+	pause();
+	move(4, 80);
+	// Score Block
+	scoreBlock();
+
+	// Drive to ramp
+
+	// Find IR Beacon
 
 
 }
