0f98d9a90779953698ad891c13b62fd7e39473c5
Logan <kai.ralston@gmail.com>
2013-12-27 15:03:06 -0500

    Update robot.c

--- robot.c
+++ robot.c
@@ -1,4 +1,25 @@
+/*!
+ * robot.c
+ * initialize the robot and build a model of it via structs
+ */
+
 #include "consts.c"
+
+// Setup robot sensors and motors
+#pragma config(Hubs, S1, HTMotor, HTServo, HTMotor, none)
+#pragma config(Sensor, S2, PaddleEOPD, sensorAnalogActive)
+#pragma config(Sensor, S3, AutoIR, sensorI2CCustom)
+#pragma config(Sensor, S4, PaddleLIGHT, sensorLightActive)
+#pragma config(Motor, mtr_S1_C1_1, LiftFlagMtr, tmotorTetrix, openLoop)
+#pragma config(Motor, mtr_S1_C1_2, PaddleMtr, tmotorTetrix, openLoop, reversed)
+#pragma config(Motor, mtr_S1_C3_1, RightDr, tmotorTetrix, openLoop, encoder)
+#pragma config(Motor, mtr_S1_C3_2, LeftDr, tmotorTetrix, openLoop, reversed)
+#pragma config(Servo, srvo_S1_C2_1, LeftIntake, tServoContinuousRotation)
+#pragma config(Servo, srvo_S1_C2_2, RightIntake, tServoContinuousRotation)
+#pragma config(Servo, srvo_S1_C2_3, servo3, tServoNone)
+#pragma config(Servo, srvo_S1_C2_4, servo4, tServoNone)
+#pragma config(Servo, srvo_S1_C2_5, servo5, tServoNone)
+#pragma config(Servo, srvo_S1_C2_6, servo6, tServoNone)
 
 // Robot's wheel
 typedef struct {
@@ -26,10 +47,9 @@ typedef struct {
 Robot robot;
 
 /**
-* Initilizes all the value and get the robot read to go
-* (also has a 0.01% change of causing world destruction)
-*/
-void initializeRobot() // a.k.a. initialize()
+ * Initilizes all the value and get the robot read to go
+ */
+void initializeRobot()
 {
 	// Set robot values
 	robot.optimalSpeed = OPTIMAL_SPEED;
