201b57367f7d2ee6300d12c67f0fafe70e03e84e
kylerNat <kylerNat@gmail.com>
2014-11-21 21:18:40 -0800

    fixed this stuff

--- teleop.c
+++ teleop.c
@@ -16,11 +16,12 @@
 //TODO: move these to actions.h
 float lift_position;//the desired lift position in cm//0 is the position at the start of teleop
 
-const float cm_per_tick = 180/ticks/PI;//the number of cm in an encoder tick
-const float lift_speed_constant = 10;//the number
+const float cm_per_tick = 180/ticks/PI;//the number of cm the lift raises when the lift motors rotate 1 encoder tick
+const float lift_speed_constant = 10.0/90.0;
+const float lift_slow_constant = 1.0/2.0;
 void updateLift(){
-	motor[liftL] = lift_speed_constant*(lift_position-nMotorEncoder[leftL]*cm_per_tick);
-	motor[liftR] = lift_speed_constant*(lift_position-nMotorEncoder[leftR]*cm_per_tick);
+    motor[liftL] = lift_speed_constant*atan(*(lift_position-nMotorEncoder[leftL]*cm_per_tick));//this atan is totally arbitrary and was only chosen because gives a good curve(the motor will be at an approx. const. speed far from the wanted point and will slow down near the wanted point)
+    motor[liftR] = lift_speed_constant*atan(*(lift_position-nMotorEncoder[leftR]*cm_per_tick));
 }
 
 #include "joystickdriver.c"
