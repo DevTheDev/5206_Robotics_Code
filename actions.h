37320c9416424e1d68640dd5d1ea851c785408bc
kylerNat <kylerNat@gmail.com>
2014-12-08 09:51:03 -0800

    modified lift sync algorithm

--- actions.h
+++ actions.h
@@ -14,16 +14,18 @@ const float lift_120 = 85.0;
 
 float lift_position = 0;//the desired lift position in cm,0 is the position at the start of teleop, max = 32.5 cm
 
+const float power_difference_per_tick = 5;
+
 const float lift_cm_per_tick = 30.0/encoderticks;//the number of cm the lift raises when the lift motors rotate 1 encoder tick
 const float lift_speed_constant = 80.0/(PI/2.0);
 const float lift_slow_constant = 1.0/20.0;
 void updateLift(){ //TODO: Add constraints on max and min
-    motor[liftL] = lift_speed_constant*atan(lift_slow_constant*(lift_position-nMotorEncoder[liftL]*lift_cm_per_tick));//this atan is totally arbitrary and was only chosen because gives a good curve(the motor will be at an approx. const. speed far from the wanted point and will slow down near the wanted point)
-    motor[liftR] = lift_speed_constant*atan(lift_slow_constant*(lift_position-nMotorEncoder[liftR]*lift_cm_per_tick));
+    int correction = nMotorEncoder[liftL] - nMotorEncoder[liftR]*power_difference_per_tick;
+    motor[liftL] = lift_speed_constant*atan(lift_slow_constant*(lift_position-nMotorEncoder[liftL]*lift_cm_per_tick)) + correction;//this atan is totally arbitrary and was only chosen because gives a good curve(the motor will be at an approx. const. speed far from the wanted point and will slow down near the wanted point)
+    motor[liftR] = lift_speed_constant*atan(lift_slow_constant*(lift_position-nMotorEncoder[liftR]*lift_cm_per_tick)) - correction;
 }
 
 const float drive_cm_per_tick = (pi*2*2.54)/encoderticks;
-const float power_difference_per_tick = 5;
 
 void driveDist(int distance, int motor_vIs){ //TODO: both motors separate
 	int correction = 0;
