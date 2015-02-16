52696f3cbb77fb860f62101075389413d72f51a6
Dev <dev.rangarajan@gmail.com>
2015-02-15 23:30:13 -0800

    Minor Fix to Sound System

--- sound tester.c
+++ sound tester.c
@@ -1,13 +1,16 @@
 /*
 Sound Tester/Loader
 
-RobotC requires the *.rso file to exist on the brick to be accessed. However,
-RobotC won't use a file system to find the sound files. (i.e. no "Sound Files/actual file.rso")
-
+RobotC requires the *.rso file to exist on the brick to be accessed.
+However, RobotC won't use a file system to find the sound files(i.e. no "Sound Files/actual file.rso".)
+To load a sound file on the brick:
+1. Move sound file into same folder as the program
+2. Download this program onto the brick
+3. Move the sound file into storage, to keep the main directory clean
 */
 
 task main()
 {
-playSoundFile("");
+playSoundFile(".rso"); //Insert sound file here
 while(bSoundActive);
 }
