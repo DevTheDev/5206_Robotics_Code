ba686655a2d52e701aeaf1aa9f022f079edf94ac
Dev <dev.rangarajan@gmail.com>
2015-04-16 12:05:27 -0700

    Started work on functions, more name fixes.

--- pathfinding.h
+++ pathfinding.h
@@ -12,7 +12,7 @@ void dot(float2 a, float2 b, float2 * out)
   out->y = a.y*b.y;
 }
 
-void subs(float2 a, float2 b, float2 * out)
+void subt(float2 a, float2 b, float2 * out)
 {
   out->x = a.x-b.x;
   out->y = a.y-b.y;
@@ -79,7 +79,7 @@ void addUSPoint(world * w, float us)
         for(int i = w->n_points-1; i >= 0; i--) //the more recent points are more likely to be close
         {
             float2 rel_pos;
-            subs(w->points[w->n_points].posit, w->points[i].posit, rel_pos);
+            subt(w->points[w->n_points].posit, w->points[i].posit, rel_pos);
             if(dotme(rel_pos) < sq(min_neighbor_radius))
             {
                 goto dont_add;
@@ -116,8 +116,8 @@ void addUSPoint(world * w, float us)
     {
         for(int n = w->points[i].n_neighbors-1; n >= 0; n--)
         {
-            float2 rel_start = subs(w->points[i].pos, us_pos);
-            float2 rel_end = subs(w->points[w->points[i].neighbors[n]].pos, us_pos);
+            float2 rel_start = subt(w->points[i].pos, us_pos);
+            float2 rel_end = subt(w->points[w->points[i].neighbors[n]].pos, us_pos);
             float y0 = dot(rel_start, w->black_knight.dir);
             float y1 = dot(rel_end, w->black_knight.dir);
             float x0 = dot(rel_start, perp(w->black_knight.dir));
@@ -125,7 +125,7 @@ void addUSPoint(world * w, float us)
 
             float d = (y1*abs(x0)+y0*abs(x1))/(abs(x1)+abs(x0));
 
-            if((US(w) != 255 || abs(dot(normalize(subs(w->points[i].posit w->points[w->points[i].neighbors[n]].posit)), w->black_knight.dir)) < max_detection_cos) && min(US(w)*inches/2.53, us_max_range) > d+max_neighbor_radius/2.0+1
+            if((US(w) != 255 || abs(dot(normalize(subt(w->points[i].posit w->points[w->points[i].neighbors[n]].posit)), w->black_knight.dir)) < max_detection_cos) && min(US(w)*inches/2.53, us_max_range) > d+max_neighbor_radius/2.0+1
                && d > 0
                && x0*x1 < 0) //if seeing through where a wall should be, then remove the wall
             {
@@ -147,13 +147,13 @@ ints[w->n_points].neighbors[n]].n_neighbors-1; o >= 0; o--)
                         if(w->points[w->point
         if(w->points[i].n_neighbors == 0 && w->points[i].neighbors[0] < 0)
         {
-            if(dot(w->black_knight.dir, normalize(subs(w->points[i].pos, us_pos))) < 0.8)
+            if(dot(w->black_knight.dir, normalize(subt(w->points[i].pos, us_pos))) < 0.8)
             {
                 w->points[i] = w->points[--w->n_points];
 
                 for(int n = w->points[w->n_points].n_neighbors-1; n >= 0; n--)
                 {
-                    for(int o = w->points[w->pos[w->n_points].neighbors[n]].neighbors[o] == w->n_points)
+                    for(int o = w->points[w->posit[w->n_points].neighbors[n]].neighbors[o] == w->n_points)
                         {
                             w->points[w->points[w->n_points].neighbors[n]].neighbors[o] = i;
                             break;
@@ -183,4 +183,64 @@ ints[w->n_points].neighbors[n]].n_neighbors-1; o >= 0; o--)
         }
     }
     #endif
+}
+float netDist(float pos_0, float pos_f)
+{
+    return pos_f - pos_0;
+}
+#if 0
+void gotox(float2 robot_position, world facing_dir, float x_coord, int motor_vIs)//Just an example for my benefit, so I can rewrite it to take a single distance. I doubt we'd want to move x and then move y.
+{
+    if(facing_dir.robot_angle != 180 || facing_dir.robot_angle != 0)//Reference angle from the north (face side), with 180 deg facing (0,0) and 0 deg facing (0, max x)
+    {
+        if(facing_dir.robot_angle >= 90 && facing_dir.robot_angle < 270)
+        {
+            collsionTurn((facing_dir.robot_angle <= 180 ? 180 - facing_dir.robot_angle : facing_dir.robot_angle - 180));//Turn while collsion avoiding to make sure robot is facing in an x direction it can move in
+        }
+        else if(facing_dir.robot_angle < 90 && facing_dir.robot_angle >= 270)
+        {
+            collsionTurn((facing_dir.robot_angle < 90 ? 0 - facing_dir.robot_angle : facing_dir.robot_angle - 360));
+        }
+    }
+    int direction = 0;
+
+    if(abs(netDist(robot_position.x, x_coord)) == netDist(robot_position.x, x_coord))//Determine which way we need to move
+    {
+        direction = 1;
+    }
+    else
+    {
+        direction = -1;
+    }
+
+    while(!isCollsionAvoiding() && abs(nMotorEncoder[driveR]*drive_cm_per_tick) <= abs(x_coord))//If there's nothing in the way, and less than the absolute distance needed to travel, keep moving
+    {
+        motor[driveL] = motor_vIs*direction;
+        motor[driveR] = motor_vIs*direction;
+    }
+    motor[driveL] = 0;
+    motor[driveR] = 0;
+}
+#endif
+
+void orientAngle(world facing_dir, float final_angle, int motor_vIs)
+{
+    if(facing_dir.robot_angle < final_angle)
+    {
+       collsionTurn(final_angle - facing_dir.robot_angle, motor_vIs);
+    }
+    else if(facing_dir.robot_angle > final_angle)
+    {
+        collsionTurn(facing_dir.robot_angle - final_angle, motor_vIs);
+    }
+    facing_dir.robot_angle = final_angle;
+}
+void gotoCoord(float2 current_posit, float x_f, float y_f, int motor_vIs)
+{
+    float distance = sqrt(sq(x_f - current_posit.x) + sq(y_f - current_posit.y));
+    if(x_f - current_posit.x < 0 || y_f - current_posit.y < 0)//If we need to go backwards at all
+    {
+        distance *= -1;
+    }
+
 }
